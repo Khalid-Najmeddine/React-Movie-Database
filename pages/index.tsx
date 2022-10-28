adf8da25061b3eabf7ad196edec99a0a01c3143d
Khalid Najmeddine <knajm574@gmail.com>
2022-10-28 12:40:59 -0600

    Client side fetching with react-query

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,7 @@
+import React from 'react';
 import type { NextPage } from 'next'
+// Fetch hook
+import { useFetchMovies } from '../API/fetchHooks';
 // Components
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
@@ -6,8 +9,11 @@ import Grid from "../components/Grid/Grid";
 import Card from "../components/Card/Card";
 import Spinner from '../components/Spinner/Spinner';
 
-
 const Home: NextPage = () => {
+  const [query, setQuery] = React.useState("");
+  const {data, fetchNextPage, isLoading, isFetching, isError} = useFetchMovies(query);
+  console.log(data);
+  
   return ( 
     <main className="relative h-screen overflow-y-scroll"> 
       <Header />
