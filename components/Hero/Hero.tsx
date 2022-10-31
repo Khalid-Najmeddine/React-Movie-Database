c7abcf374143a32686072c18928dcce9510922f0
Khalid Najmeddine <knajm574@gmail.com>
2022-10-31 16:56:08 -0600

    working codebase for Hero component

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,3 +1,21 @@
-const Hero = () => <div>Hero</div>;
+import Image from "next/image";
+
+type Props = {
+  imgUrl: string;
+  title: string;
+  text: string;
+};
+
+const Hero = ({imgUrl, title, text}: Props) => (
+  <div className="relative w-full h-128">
+    <div className="relative flex flex-col-reverse h-full max-w-7xl m-auto z-10 pb-12 text-center md:text-left">
+      <div className="text-white max-w-2xl px-4 ">
+        <h2 className="text-2xl md:text-5xl font-bold pb-8">{title}</h2>
+        <p className="text-lg md:text-xl">{text}</p>
+      </div>
+    </div>
+    <Image object-fit="cover" object-position="center" style={{objectFit: "cover", objectPosition: "center"}} priority  src={imgUrl} alt="hero-image" fill/>
+  </div>
+)
 
 export default Hero;
