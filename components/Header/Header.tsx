f52c31eeb3d861e5feb5fcac7dd181742eb2249a
Khalid Najmeddine <knajm574@gmail.com>
2022-10-31 23:18:13 -0600

    auto width & height for both <Image srcs

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -13,10 +13,10 @@ const Header = ({ setQuery}: Props) => (
       <Link href="/">
         <div className="flex items-center cursor-pointer">
           <div className="invisible md:visible">
-            <Image width="150" height="50" src="/rmdb-logo.svg" alt="rmdb-logo" />
+            <Image className="w-auto h-auto" width="150" height="50" src="/rmdb-logo.svg" alt="rmdb-logo" priority />
           </div>
           <div className="absolute md:invisible pt-2">
-            <Image width="42" height="42" src="/rmdb-logo-small.svg" alt="rmdb-logo-small" />
+            <Image width="42" height="42" src="/rmdb-logo-small.svg" alt="rmdb-logo-small" priority />
         </div>
         </div>
       </Link>
